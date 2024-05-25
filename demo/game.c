b625f54adf8cd3b01bf197865de70fdda481c41f
tcolombo@caltech.edu <tcolombo@caltech.edu>
2024-05-25 08:12:04 +0000

    Autocommit of demo or game for tcolombo

--- demo/game.c
+++ demo/game.c
@@ -147,7 +147,7 @@ state_t *emscripten_init() {
 
   // debugging collision handler
   body_t* temp = body_init_with_info(points, USER_MASS, USER_COLOR, (void *)USER_INFO, NULL);
-  body_set_centroid(temp, {100, 50});
+  body_set_centroid(temp, (vector_t){100, 50});
   scene_add_body(state -> scene, temp);
   create_physics_collision(state -> scene, state -> user_body, temp, 0);
   //
