65c95e732408c418e26fe46d4f5c7b70cf9631d3
kzhou@caltech.edu <kzhou@caltech.edu>
2024-05-28 06:04:06 +0000

    Autocommit of library for kzhou

--- library/sdl_wrapper.c
+++ library/sdl_wrapper.c
@@ -147,16 +147,7 @@ bool sdl_is_done(void *state) {
       break;
 
     case SDL_MOUSEBUTTONDOWN: {
-      bool button_clicked = asset_cache_handle_buttons(state, event->button.x, event->button.y);
-      if(!button_clicked) {
-        handle_click_drag(state, event->button.x, event->button.y);
-      }
       break;
-    }
-
-    case SDL_MOUSEBUTTONUP: {
-      handle_no_click(state);
-    }
     }
   }
   free(event);
