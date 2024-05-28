190c219c538755de3f9e90c334f0f62c5bfac65a
tcolombo@caltech.edu <tcolombo@caltech.edu>
2024-05-28 05:54:26 +0000

    Autocommit of demo or game for tcolombo

--- demo/game.c
+++ demo/game.c
@@ -171,7 +171,7 @@ bool emscripten_main(state_t *state) {
 
   scene_tick(scene, dt);
   sdl_render_scene(scene, user);
-  body_add_force(state -> user_body, (vector_t){0, -98});
+  body_add_force(state -> user_body, (vector_t){0, -980});
   body_tick(user, dt);
 
   return game_over(state);
