e6a1da2b13ef16c9975f3983e1779aa4b93ed303
tcolombo@caltech.edu <tcolombo@caltech.edu>
2024-05-28 05:15:46 +0000

    Autocommit of demo or game for tcolombo

--- demo/game.c
+++ demo/game.c
@@ -157,7 +157,7 @@ state_t *emscripten_init() {
   body_set_rotation(state->user_body, USER_ROTATION);
 
   // testing wall collisions
-  body_add_force(state -> user_body, (vector_t){0, -9.8});
+  
   body_set_velocity(state -> user_body, (vector_t){100, 50});
 
   state->game_over = false;
@@ -171,7 +171,7 @@ bool emscripten_main(state_t *state) {
 
   scene_tick(scene, dt);
   sdl_render_scene(scene, user);
-  
+  body_add_force(state -> user_body, (vector_t){0, -9.8});
   body_tick(user, dt);
 
   return game_over(state);
