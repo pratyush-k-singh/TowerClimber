4cd111f33b4bede5d66471853206e6e37738055d
kzhou@caltech.edu <kzhou@caltech.edu>
2024-05-31 00:57:45 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -57,7 +57,7 @@ const char *PLATFORM_INFO = "platform";
 
 // Game constants
 const size_t NUM_LEVELS = 1;
-const double GRAVITY = -980;
+const vector_t GRAVITY = {0, -980};
 const size_t BODY_ASSETS = 3; // 2 walls and 1 platform
 
 struct state {
