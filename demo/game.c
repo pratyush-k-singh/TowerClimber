8d511765563050c931a79e3558e68e09edc2ff81
kzhou@caltech.edu <kzhou@caltech.edu>
2024-05-31 01:27:57 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -237,11 +237,11 @@ void sticky_collision(state_t *state, body_t *body1, body_t *body2){
     if (state->is_jumping) {
     body_set_velocity(body1, VEC_ZERO);
     body_set_velocity(body2, VEC_ZERO);
-    state->is_jumping = false;}
-    // } else if (strcmp(body_get_info(body2), PLATFORM_INFO)) {
-    //   body_set_velocity(body1, (vector_t) {v1.x, 0});
-    //   printf("yay");
-    // }
+    state->is_jumping = false;
+    } else if (strcmp(body_get_info(body2), PLATFORM_INFO) == 0) {
+      body_set_velocity(body1, (vector_t) {v1.x, 0});
+      printf("yay");
+    }
   }
 }
 
