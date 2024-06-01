741a9fd5c10212ab6a549aaa861dffdbd029d10f
kzhou@caltech.edu <kzhou@caltech.edu>
2024-06-01 05:10:08 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -232,15 +232,17 @@ void sticky_collision(state_t *state, body_t *body1, body_t *body2){
   vector_t v1 = body_get_velocity(body1);
   vector_t v2 = body_get_velocity(body2);
   state -> collided = find_collision(body1, body2).collided;
-  bool velocity_zero = (vec_cmp(v1, VEC_ZERO) && vec_cmp(v2, VEC_ZERO));
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! HELLO READ ME PLEASE :D !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  bool velocity_zero = (vec_cmp(v1, VEC_ZERO) && vec_cmp(v2, VEC_ZERO)); // What does this line do lol :D
   if (state -> collided && !velocity_zero){
     body_set_velocity(body1, VEC_ZERO);
     body_set_velocity(body2, VEC_ZERO);
     state->is_jumping = false;
-    if (strcmp(body_get_info(body2), PLATFORM_INFO) == 0) {
-        //body_set_velocity(body1, (vector_t) {v1.x * FRICTION, 0});
-        body_set_velocity(body1, (vector_t) {v1.x, 0});
-    }
+    // if (strcmp(body_get_info(body2), PLATFORM_INFO) == 0) {
+    //     //body_set_velocity(body1, (vector_t) {v1.x * FRICTION, 0});
+    //     //body_set_velocity(body1, (vector_t) {v1.x, 0});
+
+    // }
   }
 }
 
