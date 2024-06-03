0cf0a454ebd18dd1b6011b0f677f25833ca7e117
kzhou@caltech.edu <kzhou@caltech.edu>
2024-06-03 20:52:31 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -322,7 +322,7 @@ void jump_powerup_collision(state_t *state, body_t *body1, body_t *body_2) {
 /**
  * TO DO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
-void health_powerup_collision(state_t *state, body_t *body1, body_t *body_2, vector_t VEC_ZERO, void *aux, double force_const) {
+void *health_powerup_collision(state_t *state, body_t *body1, body_t *body_2, vector_t VEC_ZERO, void *aux, double force_const) {
   body_remove(body_2);
       
       if (state->user_health < 3) {
