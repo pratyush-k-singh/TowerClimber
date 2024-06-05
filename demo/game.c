dd985d58816e7f948cbf72e1f346e0eb82beb5f5
kzhou@caltech.edu <kzhou@caltech.edu>
2024-06-05 17:49:12 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -555,9 +555,11 @@ bool emscripten_main(state_t *state) {
   for (size_t i = 0; i < scene_bodies(scene); i++){
     body_t *body = scene_get_body(scene, i);
 
-    if (collision(state, user, body)) {
+    bool collision_success = collision(state, user, body)
+    if (collision_success) {
       i--;
     }
+  
     printf("loop through bodies\n");
     printf("%d", get_type(body)); // never got to 4
 
