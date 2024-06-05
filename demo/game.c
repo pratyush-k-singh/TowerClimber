46bc2422b94542a9720369c51263015fecfca643
kzhou@caltech.edu <kzhou@caltech.edu>
2024-06-05 04:43:50 +0000

    Autocommit of game for kzhou

--- demo/game.c
+++ demo/game.c
@@ -353,9 +353,7 @@ void check_jump_off(state_t *state) {
  * @param body1 the user
  * @param body2 the body with which the user is colliding
  */
-void sticky_collision(body_t *body1, body_t *body2, vector_t axis, void *aux,
-                double force_const){
-  state_t *state = aux;
+void sticky_collision(state_t *state, body_t *body1, body_t *body2){
   vector_t v1 = body_get_velocity(body1);
   vector_t v2 = body_get_velocity(body2);
   state -> collided = find_collision(body1, body2).collided;
@@ -398,40 +396,40 @@ void powerup_collision(body_t *body1, body_t *body2, vector_t axis, void *aux,
   }
 }
 
-/**
- * Adds collision handler force creators between appropriate bodies.
- *
- * @param state the current state of the demo
- */
-void add_force_creators(state_t *state) { 
-  for (size_t i = 0; i < scene_bodies(state->scene); i++) {
-    body_t *body = scene_get_body(state->scene, i);
-    switch (get_type(body)) {
-    case LEFT_WALL:
-      create_collision(state->scene, state->user_body, body,
-                       (collision_handler_t)sticky_collision, state, 0);
-      break;
-    case RIGHT_WALL:
-      create_collision(state->scene, state->user_body, body,
-                       (collision_handler_t)sticky_collision, state, 0);
-      break;
-    case PLATFORM:
-      create_collision(state->scene, state->user_body, body,
-                       (collision_handler_t)sticky_collision, state, 0);
-      break;
-    case JUMP_POWER:
-      create_collision(state->scene, state->user_body, body,
-                       (collision_handler_t)powerup_collision, state, 0);
-      break;
-    case HEALTH_POWER:
-      create_collision(state->scene, state->user_body, body,
-                       (collision_handler_t)powerup_collision, state, 0);
-      break;
-    default:
-      break;
-    }
-  }
-}
+// /**
+//  * Adds collision handler force creators between appropriate bodies.
+//  *
+//  * @param state the current state of the demo
+//  */
+// void add_force_creators(state_t *state) { 
+//   for (size_t i = 0; i < scene_bodies(state->scene); i++) {
+//     body_t *body = scene_get_body(state->scene, i);
+//     switch (get_type(body)) {
+//     case LEFT_WALL:
+//       create_collision(state->scene, state->user_body, body,
+//                        (collision_handler_t)sticky_collision, state, 0);
+//       break;
+//     case RIGHT_WALL:
+//       create_collision(state->scene, state->user_body, body,
+//                        (collision_handler_t)sticky_collision, state, 0);
+//       break;
+//     case PLATFORM:
+//       create_collision(state->scene, state->user_body, body,
+//                        (collision_handler_t)sticky_collision, state, 0);
+//       break;
+//     case JUMP_POWER:
+//       create_collision(state->scene, state->user_body, body,
+//                        (collision_handler_t)powerup_collision, state, 0);
+//       break;
+//     case HEALTH_POWER:
+//       create_collision(state->scene, state->user_body, body,
+//                        (collision_handler_t)powerup_collision, state, 0);
+//       break;
+//     default:
+//       break;
+//     }
+//   }
+// }
 
 /**
  * Move player on display screen based on key pressed.
@@ -532,7 +530,7 @@ state_t *emscripten_init() {
   create_health_power_up(state);
   create_jump_power_up(state);
 
-  add_force_creators(state);
+  //add_force_creators(state);
 
   sdl_on_key((key_handler_t)on_key);
 
@@ -569,7 +567,7 @@ bool emscripten_main(state_t *state) {
   for (size_t i = 0; i < scene_bodies(scene); i++){
     body_t *body = scene_get_body(scene, i);
 
-    sticky_collision(user, body, VEC_ZERO, state, WALL_ELASTICITY);
+    sticky_collision(state, user, body);
 
     // include gravity
     if (!find_collision(state -> user_body, body).collided && get_type(body) == PLATFORM){
