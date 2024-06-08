214e07be18cf2cd0673818b277b2afa0aab1ee39
tcolombo@caltech.edu <tcolombo@caltech.edu>
2024-06-08 10:18:46 +0000

    Autocommit of game for tcolombo

--- demo/game.c
+++ demo/game.c
@@ -155,6 +155,8 @@ struct state {
   
   bool jumping; // determines whether up button can be pressed
   body_t *collided_obj; // the object that the user is collided with
+  double wall_r;
+  double wall_l;
   
   size_t jump_powerup_jumps;
 
@@ -464,6 +466,11 @@ void sticky_collision(body_t *body1, body_t *body2, vector_t axis, void *aux,
   }
 
   state->colliding_buffer = 0;
+  if (get_type(body2) == LEFT_WALL){
+    state->wall_l = 0;
+  } else if (get_type(body2) == RIGHT_WALL){
+    state->wall_r = 0;
+  }
 }
 
 /**
@@ -708,16 +715,18 @@ void on_key(char key, key_event_type_t type, double held_time, state_t *state) {
   vector_t cur_v = body_get_velocity(user);
   double new_vx = cur_v.x;
   double new_vy = cur_v.y;
+  bool colliding_r = (state->wall_r < COLLIDING_BUFFER);
+  bool colliding_l = (state->wall_l < COLLIDING_BUFFER);
+
   if (type == KEY_PRESSED) {
-      switch (key) {
       case LEFT_ARROW: {
-        if (get_type(state->collided_obj) != LEFT_WALL) {
+        if (get_type(state->collided_obj) != LEFT_WALL && !colliding) {
           new_vx = -1 * (RESTING_SPEED + ACCEL * held_time);
         }
         break;
       }
       case RIGHT_ARROW: {
-        if (get_type(state->collided_obj) != RIGHT_WALL) {
+        if (get_type(state->collided_obj) != RIGHT_WALL && !colliding) {
           new_vx = RESTING_SPEED + ACCEL * held_time;
         }
         break;
@@ -737,6 +746,20 @@ void on_key(char key, key_event_type_t type, double held_time, state_t *state) {
     }
   }
   body_set_velocity(user, (vector_t) {new_vx, new_vy});
+}
+
+/**
+ * Updates all the buffers in the main loop
+ * 
+ * @param dt the time between each tick
+ */
+void update_buffers(state_t *state, double dt){
+  state->ghost_timer += dt;
+  state->velocity_timer += dt;
+  state->user_immunity += dt;
+  state->colliding_buffer += dt;
+  state->wall_l += dt;
+  state->wall_r += dt;
 }
 
 
@@ -786,6 +809,8 @@ state_t *emscripten_init() {
 
   // Intialize walls and platforms
   create_walls_and_platforms(state);
+  state->wall_l = 0;
+  state->wall_r = 0;
 
   // Initialize powerups
   create_health_power_up(state);
@@ -811,10 +836,8 @@ state_t *emscripten_init() {
 
 bool emscripten_main(state_t *state) {
   double dt = time_since_last_tick();
-  state->ghost_timer += dt;
-  state->velocity_timer += dt;
-  state->user_immunity += dt;
-  state->colliding_buffer += dt;
+  update_buffers(state, dt);
+  
   body_t *user = state->user;
   scene_t *scene = state->scene;
   scene_tick(scene, dt);
