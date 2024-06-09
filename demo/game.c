5b932782880a5f7afa6e86639500116f3500faa6
tcolombo@caltech.edu <tcolombo@caltech.edu>
2024-06-09 08:20:19 +0000

    Fixed obstacles

--- demo/game.c
+++ demo/game.c
@@ -260,7 +260,7 @@ list_t *make_user(vector_t center, void *info, size_t idx) {
     size_t position = idx % (SPIKE_NUM / NUM_LEVELS);
     double x = WALL_WIDTH.x + SPIKE_RADIUS * pow((-1), position + 1)
              + GAP_DISTANCE * (1 - position);
-    center_body = (vector_t){x, y};
+    center_body = (vector_t){x, y}; //first spike coorder: (700, 800)
   }
   list_t *c = list_init(USER_NUM_POINTS, free);
   for (size_t i = 0; i < USER_NUM_POINTS; i++) {
